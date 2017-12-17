81608b8f6652a076630f11cdfaf742ba49aca0e2
Tim Skirvin <tskirvin@killfile.org>
2017-12-17 21:02:39 +0000

    debian - putting the scripts in the .deb, still need man pages

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ import glob
 pyfiles = glob.glob(os.path.join('*', '*.py'))
 pyfiles = [pyfile[:-3] for pyfile in pyfiles]
 
+scripts = glob.glob(os.path.join('usr/bin/*'))
+# still need to build the manpages
+
 setup (
   name             = 'omdclient',
   version          = '1.0.0',
@@ -13,6 +16,8 @@ setup (
   maintainer       = 'Tim Skirvin',
   maintainer_email = 'tskirvin@fnal.gov',
   package_dir      = { 'omdclient': 'omdclient' },
-  url              = 'http://cms-git.fnal.gov/omdclient',
+  scripts = scripts,
+  url              = 'https://github.com/tskirvin/omdclient',
   py_modules       = pyfiles,
 )
+
